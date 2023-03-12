76ea9532f88646489db3c11f55c857b0c91c92e2
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2023-03-12 08:27:22 -0300

    Update app1.sh

--- app1.sh
+++ app1.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
+#!/bin/bash
 
-while true; do
-    clear
-    echo "------------------------"
-    echo "Selecione uma opção:"
-    echo "1. Processos mais pesados"
-    echo "2. Portas mais utilizadas"
-    echo "3. Velocidade da internet"
-    echo "4. Informações de IP"
-    echo "5. Tráfego de rede em tempo real"
-    echo "6. Contador de desligamento do computador"
-    echo "0. Sair"
-    echo "------------------------"
-    read option
+opcoes=("5 minutos" "10 minutos" "15 minutos" "30 minutos" "1 hora" "2 horas" "3 horas")
+tempos=(300 600 900 1800 3600 7200 10800)
 
-    case $option in
-        1) ps -e --sort=-%mem --format="%mem %cpu %pid %comm" | head -n 6 ;;
-        2) netstat -atn | awk '{print $4}' | cut -d':' -f2 | sort | uniq -c | sort -nr | head -n 5 ;;
-        3) speedtest-cli --simple | grep -E "Ping|Download|Upload" ;;
-        4) echo "Endereço IP da máquina: $(hostname -I)"; echo "Endereço IP do roteador: $(ip route | grep default | awk '{print $3}')" ;;
-        5) sudo iftop ;;
-        6) zenity --info --title="Contador de Desligamento" --text="O computador será desligado em 60 minutos. Salve seu trabalho agora!" --timeout=5; for ((i=3600;i>0;i--)); do echo "#$((100-(i*100/3600)))%"; echo $((100-(i*100/3600))) | zenity --progress --title="Contador de Desligamento" --text="O computador será desligado em $i segundos. Pressione Cancelar para interromper o processo." --percentage=$((100-(i*100/3600))) --auto-close --auto-kill --width=300 --height=100 || { zenity --info --title="Contador de Desligamento" --text="Desligamento interrompido pelo usuário."; exit 1; }; done; zenity --info --title="Contador de Desligamento" --text="Desligando o computador..."; sudo shutdown -h now ;;
-        0) exit ;;
-        *) echo "Opção inválida! Tente novamente." ;;
-    esac
-    read -p "Pressione Enter para continuar"
+echo "Escolha uma opção de tempo para o desligamento:"
+select opcao in "${opcoes[@]}"
+do
+    if [[ " ${opcoes[@]} " =~ " ${opcao} " ]]; then
+        index=$((${!opcoes[@]}+1))
+        tempo_em_segundos=${tempos[$index-1]}
+        echo "O sistema será desligado em ${opcao}."
+        break
+    else
+        echo "Opção inválida. Escolha um número entre 1 e ${#opcoes[@]}."
+    fi
 done
+
+shutdown -s -t $tempo_em_segundos
